6288a69b48f1fa32148615d85181392bd058fda9
Kavitha B S <kavitha.bs@experionglobal.local>
2023-01-21 16:37:12 +0530

    fifth commit

---
+++ C#_experiments/questions/qust12/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+Console.WriteLine("enter the passenger names");
+string[] name = new string[5];
+for (int i = 0; i < 5; i++)
+{
+    name[i] = Console.ReadLine();
+}
+Console.WriteLine("enter the destinations");
+string[] dest = new string[5];
+for (int i = 0; i < 5; i++)
+{
+    dest[i] = Console.ReadLine();
+}
+Console.WriteLine("merged array is");
+string[] merge_array = new string[name.Length + dest.Length];
+
+int j = 0;
+for (int i = 0; i < name.Length; i++)
+{
+    merge_array[j] = name[i];
+    merge_array[j + 1] = dest[i];
+    j = j + 2;
+
+}
+    //int j = 0;
+    //for(int i =0; i < name.Length; i++)
+    //{
+    //    merge_array[i] = name[i];
+    //    j++;
+    //}
+    //int k = 0;
+    //for (int l = 0; l < dest.Length; l++)
+    //{
+    //    merge_array[k] = dest[l];
+    //    k++;
+    //}
+
+ for (int i = 0; i < merge_array.Length; i++)
+ {
+    Console.WriteLine(merge_array[i]);
+ }
---
+++ C#_experiments/questions/qust15/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr = new int[10];
+
+for (int i = 0; i < 5; i++)
+
+{
+    Console.WriteLine("enter weight");
+
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+
+    if (arr[i] > 23)
+    {
+        var total = (arr[i] - 23) * 15;
+        Console.WriteLine($"overweight fare total={total}");
+
+    }
+    else
+    {
+        Console.WriteLine("no overwheight");
+    }
+}
+Console.WriteLine("sorted order :");
+for (int i = 0; i < 5; i++)
+{
+    for (int j = i + 1; j < 5; j++)
+    {
+        if (arr[i] < arr[j])
+        {
+             int temp = arr[i];
+             arr[i] = arr[j];
+             arr[j] = temp;
+        }
+    }
+}
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"{arr[i]}");
+}
